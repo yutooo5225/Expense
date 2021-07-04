5ef158d70be9a15b5ab002404e096556791c8781
Henry <57031298+yutooo5225@users.noreply.github.com>
2021-07-04 23:48:13 +0800

    Add Form
    - add form with style
    - pass data from children to parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
   const expenses = [
@@ -30,9 +31,14 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   )
