1814fd837fe58ac02c518f75efbf17330d25d210
Henry <57031298+yutooo5225@users.noreply.github.com>
2021-07-06 23:33:13 +0800

    Add ExpenseList conditionally
    add conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021')
@@ -22,15 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {filteredExpense.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpense} />
       </Card>
     </div>
   )
