2a5fbdca3d41765b1c603011d3c0e04c89cf0576
Henry <57031298+yutooo5225@users.noreply.github.com>
2021-07-13 00:04:26 +0800

    add Chart with style

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card'
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021')
@@ -22,6 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpense}/>
         <ExpensesList items={filteredExpense} />
       </Card>
     </div>
